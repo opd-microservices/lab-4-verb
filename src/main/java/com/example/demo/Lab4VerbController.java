a1efa1a4e1aef12b8b690896989d56c2b6131be2
Osmand Das <osmandpdas@gmail.com>
2019-01-06 23:55:28 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Lab4VerbController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Lab4VerbController {
+	
+	@Value("${words}")
+	String words;
+	
+	@GetMapping("/")
+	  public @ResponseBody String getWord() {
+	    String[] wordArray = words.split(",");
+	    int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+	    System.out.println("Random word selected in Lab4VerbController: wordArray[" + i + "] = " +  wordArray[i]);
+	    return wordArray[i];
+	  }
+}
